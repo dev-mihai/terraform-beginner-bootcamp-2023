d55a0605748592084e2d6529753043052ceb497a
Mihai <popa.mihail.n@gmail.com>
2023-09-30 17:31:49 +0000

    try and fix our issue

--- modules/terrahouse_aws/main.tf
+++ modules/terrahouse_aws/main.tf
@@ -34,7 +34,7 @@ resource "aws_s3_bucket_website_configuration" "website_configuration" {
 resource "aws_s3_object" "index_html" {
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "index.html"
-  source = var.index_html_filepath
+  source = "${path.root}${var.index_html_filepath}"
 
   etag = filemd5(var.index_html_filepath)
 }
@@ -43,7 +43,7 @@ resource "aws_s3_object" "index_html" {
 resource "aws_s3_object" "error_html" {
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "error.html"
-  source = var.error_html_filepath
+  source = "${path.root}${var.error_html_filepath}"
 
   etag = filemd5(var.error_html_filepath)
 }
