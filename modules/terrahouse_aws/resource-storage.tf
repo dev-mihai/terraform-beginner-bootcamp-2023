387194b2b75a8a1bd089e264e670024b88538976
Mihai <popa.mihail.n@gmail.com>
2023-10-02 19:05:14 +0000

    #31 only upload index.html if a version is changed

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -3,7 +3,6 @@ resource "aws_s3_bucket" "website_bucket" {
   # Bucket Naming Rules
   #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
   bucket = var.bucket_name
-
   tags = {
     UserUuid = var.user_uuid
   }
@@ -12,11 +11,9 @@ resource "aws_s3_bucket" "website_bucket" {
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_website_configuration
 resource "aws_s3_bucket_website_configuration" "website_configuration" {
   bucket = aws_s3_bucket.website_bucket.bucket
-
   index_document {
     suffix = "index.html"
   }
-
   error_document {
     key = "error.html"
   }
@@ -27,8 +24,11 @@ resource "aws_s3_object" "index_html" {
   key    = "index.html"
   source = var.index_html_filepath
   content_type = "text/html"    
-
   etag = filemd5(var.index_html_filepath)
+  lifecycle {
+    replace_triggered_by = [terraform_data.content_version.output]
+    ignore_changes = [etag]
+  }  
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
@@ -37,8 +37,10 @@ resource "aws_s3_object" "error_html" {
   key    = "error.html"
   source = var.error_html_filepath
   content_type = "text/html"    
-
   etag = filemd5(var.error_html_filepath)
+  # lifecycle {
+  #   ignore_changes = [etag]
+  # }
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
@@ -62,4 +64,8 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
       }
     }
   })
+}
+
+resource "terraform_data" "content_version" {
+  input = var.content_version
 }
