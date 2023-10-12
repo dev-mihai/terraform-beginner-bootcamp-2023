3fe05ce14a563186ad4ef8bef067b3f2c23203a5
Mihai <popa.mihail.n@gmail.com>
2023-10-12 22:08:09 +0000

    #48 fix issues with provider, setup two different towns

--- main.tf
+++ main.tf
@@ -13,13 +13,12 @@ terraform {
   #    name = "terra-house-1"
   #  }
   #}
-  #cloud {
-  #  organization = "ExamPro"
-  #  workspaces {
-  #    name = "terra-house-1"
-  #  }
-  #}
-
+  cloud {
+   organization = "devpopa"
+   workspaces {
+     name = "terra-house-mihai"
+   }
+  }
 }
 
 provider "terratowns" {
@@ -28,23 +27,38 @@ provider "terratowns" {
   token = var.terratowns_access_token
 }
 
-module "terrahouse_aws" {
-  source = "./modules/terrahouse_aws"
+module "home_juventus_hosting" {
+  source = "./modules/terrahome_aws"
   user_uuid = var.teacherseat_user_uuid
   bucket_name = var.bucket_name
-  index_html_filepath = var.index_html_filepath
-  error_html_filepath = var.error_html_filepath
-  content_version = var.content_version
-  assets_path = var.assets_path
+  public_path = var.juventus.public_path
+  content_version = var.juventus.content_version
 }
 
 resource "terratowns_home" "home" {
-  name = "How to play Arcanum in 2023!!"
+  name = "Juventus FC"
   description = <<DESCRIPTION
 An Italian Icon since 1897
 DESCRIPTION
-  #domain_name = "3423ds.cloudfront.net"
-  domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = module.home_juventus_hosting.domain_name
+  town = "missingo"
+  content_version = var.juventus.content_version
+}
+
+module "home_partitionguide_hosting" {
+  source = "./modules/terrahome_aws"
+  user_uuid = var.teacherseat_user_uuid
+  public_path = var.partitionguide.public_path
+  bucket_name = var.bucket_name
+  content_version = var.partitionguide.content_version
+}
+
+resource "terratowns_home" "home_partitionguide" {
+  name = "How to Increase the Size of the Root Partition"
+  description = <<DESCRIPTION
+This page provides a structured guide on how to increase the size of the root (/) partition, specifically when it's an LVM logical volume.
+DESCRIPTION
+  domain_name = module.home_partitionguide_hosting.domain_name
   town = "missingo"
   content_version = 1
 }
--- variables.tf
+++ variables.tf
@@ -14,19 +14,16 @@ variable "bucket_name" {
  type = string
 }
 
-variable "index_html_filepath" {
-  type = string
+variable "juventus" {
+  type = object({
+    public_path = string
+    content_version = number
+  })
 }
 
-variable "error_html_filepath" {
-  type = string
-}
-
-variable "content_version" {
-  type        = number
-}
-
-variable "assets_path" {
-  description = "Path to assets folder"
-  type = string
+variable "partitionguide" {
+  type = object({
+    public_path = string
+    content_version = number
+  })
 }
